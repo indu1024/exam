cd43c0d0baba5e492ecfc707c92cc268f6fc5ecf
Indumathi <ikarnati24@gmail.com>
2022-11-23 11:48:24 +0530

    demo

---
+++ src/main/java/com/SampleMaven/samplemavenexam/App.java
@@ -0,0 +1,13 @@
+package com.SampleMaven.samplemavenexam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE lab internal 1 by Indumathi" );
+    }
+}
